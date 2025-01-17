fb70f0a88f30483a420ed59311343bd3224c4896
Sangani Akshay <114087974+Akki-patel@users.noreply.github.com>
2025-01-17 19:10:40 +0530

    Update UpdateBook.jsx

--- frontend/src/pages/DashBoard/EditBook/UpdateBook.jsx
+++ frontend/src/pages/DashBoard/EditBook/UpdateBook.jsx
@@ -3,7 +3,7 @@ import InputField from '../addBook/InputField'
 import SelectField from '../addBook/SelectField'
 import { useForm } from 'react-hook-form';
 import { useParams } from 'react-router-dom';
-import { useFetchBookByIdQuery, useUpdatebookMutation } from '../../../redux/features/books/booksApi';
+import { useFetchBookByIdQuery, useUpdatebookMutation } from '../../../redux/Features/books/booksApi';
 import Loading from '../../../components/Loading';
 import Swal from 'sweetalert2';
 import axios from 'axios';
